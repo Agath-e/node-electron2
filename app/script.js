233d439ed462e956c4824ba71398be3107e52c9a
Agata Pacholczyk <agataa.pacholczyk@gmail.com>
2020-07-30 17:15:21 +0200

    add all task protect your eyes [20.4]

---
+++ app/script.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+
+  state = {
+    status: 'off',
+    time: 0,
+    timer: null,
+  }
+
+  formatTime = time => {
+
+    let formatTime;
+    let minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time - (minutes * 60));
+
+    if(minutes < 10) {
+      minutes = '0' + minutes;
+    }
+
+    if(seconds < 10) {
+      seconds = '0' + seconds;
+    }
+
+    formatTime = minutes + ' : ' + seconds;
+
+    return formatTime;
+  }
+
+  step = () => {
+    this.setState({ time: this.state.time - 1 });
+    if (this.state.time === 0) {
+      this.setState({
+        status: this.state.status === 'work' ? 'rest' : 'work',
+        time: this.state.status === 'work' ? 20 : 1200,
+      });
+      this.playBell();
+    }
+  };
+
+  startTimer = () => {
+
+    this.setState({
+      status: 'work',
+      time: 1200,
+      timer: setInterval(this.step, 1000),
+    });
+
+  };
+
+  stopTimer = () => {
+
+    clearInterval(this.state.timer);
+    this.setState({
+      status: 'off',
+      time: 0,
+      
+    });
+
+  }
+  
+  closeApp = () => {
+    window.close();
+  }
+
+  playBell = () => {
+    const bell = new Audio('./sounds/bell.wav');
+    bell.play();
+  };
+
+  render() {
+
+    const { status } = this.state;
+
+    return (
+      <div>
+        <h1>Protect your eyes</h1>
+        {(status === "off") && <div>
+          <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
+          <p>This app will help you track your time and inform you when it's time to rest.</p>
+        </div>}
+        {(status === "work") && <img src="./images/work.png" />}
+        {(status === "rest") && <img src="./images/rest.png" />}
+        {(status !== "off") && <div className="timer">
+          {this.formatTime(this.state.time)}
+        </div>}
+        {(status === "off") && <button className="btn" onClick={this.startTimer}>Start</button>}
+        {(status !== "off") && <button className="btn" onClick={this.stopTimer}>Stop</button>}
+        <button className="btn btn-close" onClick={this.closeApp}>X</button>
+      </div>
+    )
+  }
+};
+
+render(<App />, document.querySelector('#app'));
